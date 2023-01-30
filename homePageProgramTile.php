10f515cd5b8212a12217a9391f713f03c7aeab18
Ramin <farhadiramin91@gmail.com>
2023-01-30 12:29:39 -0500

    HomePageProgramTile
    It was seprated into homepageProgramTile.php then included to home page. In order to load css, it homePageProgramTile.css must be added. For mor info please take a look at readme file

---
+++ homePageProgramTile.php
@@ -0,0 +1,160 @@
+<link rel="stylesheet" href="./homePageProgramTile.css">
+
+<div class="container-fluid mainTiles">
+      <div class="backImg"></div>
+      <div class="row" style="padding-bottom: 15px">
+        <div class="col-sm-6 col-md-4 tileCard">
+          <a href="https://gst.touro.edu/academics/instructional-technology/index.php">
+            <div class="ptile">
+              <div class="childTile">
+                <img  class='tileImage' src="./images/instructionalTech.jpg" alt="">
+                <div class='tileWords'>
+                  <h3 class='tileTitle'>
+                  INSTRUCTIONAL <br>TECHNOLOGY
+                  </h3>
+                  <ul class="tileLinks">
+                    <li><a href="https://gst.touro.edu/academics/instructional-technology/index.php">Masters</a></li>
+                  </ul>
+                </div>
+              </div>
+            </div>
+          </a>
+        </div>
+        <div class="col-sm-6 col-md-4 tileCard">
+          <a href="https://gst.touro.edu/academics/web-and-multimedia-design.php">
+            <div class="ptile">
+              <div class="childTile">
+                <img  class='tileImage' src="./images/wmmDesign.jpg" alt="">
+                <div class='tileWords'>
+                  <h3 class='tileTitle'>
+                  WEB & MULTIMEDIA <br> DESIGN
+                  </h3>
+                  <ul class="tileLinks">
+                    <li><a href='https://gst.touro.edu/academics/web-and-multimedia-design.php'>Masters</a></li>
+                    <li><a href='https://gst.touro.edu/academics/wmm-advanced-certificate.php'>Adv Certificate</a></li>
+                  </ul>
+                </div>
+              </div>
+            </div>
+          </a>
+        </div>
+        <div class="col-sm-6 col-md-4 tileCard">
+          <div class="ptile">
+            <a href="https://gst.touro.edu/academics/data-analytics/index.php">
+              <div class="childTile">
+                <img  class='tileImage' src="./images/dataAnalytics.jpg" alt="">
+                <div class='tileWords'>
+                  <h3 class='tileTitle'>
+                  DATA <br> ANALYTICS
+                  </h3>
+                  <ul class="tileLinks">
+                    <li><a href="https://gst.touro.edu/academics/data-analytics/index.php">Masters</a></li>
+                  </ul>
+                </div>
+              </div>
+            </div>
+          </a>
+        </div>
+        <div class="col-sm-6 col-md-4 tileCard">
+           <a href="https://gst.touro.edu/academics/information-systems/network-administration-and-security.php">
+              <div class="ptile">
+                <div class="childTile">
+                  <img  class='tileImage' src="./images/cyberSecurity.jpg" alt="">
+                  <div class='tileWords'>
+                    <h3 class='tileTitle'>
+                    CYBER SECURITY <br> & NETWORK ADMIN
+                    </h3>
+                    <ul class="tileLinks">
+                      <li><a href="https://gst.touro.edu/academics/information-systems/network-administration-and-security.php">Masters</a></li>
+                    </ul>
+                  </div>
+                </div>
+              </div>
+            </a> 
+        </div>
+        <div class="col-sm-6 col-md-4 tileCard">
+        <a href="https://gst.touro.edu/academics/information-systems/web-and-application-development.php">
+            <div class="ptile">
+              <div class="childTile">
+                <img  class='tileImage' src="./images/webApp.jpg" alt="">
+                <div class='tileWords'>
+                  <h3 class='tileTitle'>
+                  WEB & APP<br>DEVELOPMENT
+                  </h3>
+                  <ul class="tileLinks">
+                    <li><a href="https://gst.touro.edu/academics/information-systems/web-and-application-development.php">Masters</a></li>
+                  </ul>
+                </div>
+              </div>
+            </div>
+          </a>
+        </div>
+        <div class="col-sm-6 col-md-4 tileCard">
+          <div class="ptile">
+            <a href="https://gst.touro.edu/academics/information-systems/technology-leadership-project-management.php">
+              <div class="childTile">
+                  <img  class='tileImage' src="./images/techLeaderShip.jpg" alt="">
+                  <div class='tileWords'>
+                    <h3 class='tileTitle'>
+                    TECH LEADERSHIP /<br>PROJECT MANAGEMENT
+                    </h3>
+                    <ul class="tileLinks">
+                      <li><a href="https://gst.touro.edu/academics/information-systems/technology-leadership-project-management.php">Masters</a></li>
+                      <li><a href='https://gst.touro.edu/academics/project-management-advanced-certificate.php'>Adv Certificate</a></li>
+                    </ul>
+                  </div>
+              </div>
+            </a>  
+          </div>
+        </div>
+        <div class="col-sm-6 col-md-4 tileCard">
+          <div class="ptile">
+            <a href="https://gst.touro.edu/academics/data-analytics/business.php">
+              <div class="childTile">
+                <img  class='tileImage' src="./images/businessIntel.jpg" alt="">
+                <div class='tileWords'>
+                  <h3 class='tileTitle'>
+                  BUSINESS INTEL <br>& DATABASE SYSTEMS
+                  </h3>
+                  <ul class="tileLinks">
+                    <li><a href="https://gst.touro.edu/academics/data-analytics/business.php">Masters</a></li>
+                  </ul>
+                </div>
+              </div>
+            </a>  
+          </div>
+        </div>
+        <div class="col-sm-6 col-md-4 tileCard">
+          <a href="https://gst.touro.edu/academics/information-systems/index.php">
+            <div class="ptile">
+              <div class="childTile">
+                <img  class='tileImage' src="./images/informationSystems.jpg" alt="">
+                <div class='tileWords'>
+                  <h3 class='tileTitle'>
+                  INFORMATION <br>SYSTEMS
+                  </h3>
+                  <ul class="tileLinks">
+                    <li><a href="https://gst.touro.edu/academics/information-systems/index.php">Masters</a></li>
+                  </ul>
+                </div>
+              </div>
+            </div>
+          </a>  
+        </div>
+        <div class="col-sm-6 col-md-4 tileCard">
+          <div class="ptile">
+            <div class="childTile">
+              <div class='tileSign'>
+                <p> In Person <br> Online <br> & Blended <br> Programs</p>
+              </div>
+              <div class='tileSignbtns'>
+                <button class='btn' onClick="window.location.href='https://tourooneconnect.touro.edu/custom-portal/user-profile/login?cmcps_location=Touro%20College&cmcps_college=Graduate%20School%20of%20Technology'"> Apply</button>
+                <button class='btn' onClick="window.location.href='https://gst.touro.edu/academics/index.php'">Learn More</button>
+              </div>
+            </div>
+          </div>
+        </div>
+
+      </div>
+     
+    </div>
